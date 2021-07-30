236bd91b29b2f954347a390e75296945e46cc136
zzg <321719655@qq.com>
2021-07-30 22:03:13 +0800

    第二版

--- src/main/java/com/tutu/git.java
+++ src/main/java/com/tutu/git.java
@@ -3,6 +3,7 @@ package com.tutu;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello.gi树收缩t");
+        System.out.println("hello.gi树收缩t");
     }
 
 }
