fc604ce3a21b2d6deb66b6fd621dfe5cc72df2f4
zzg <321719655@qq.com>
2021-07-30 23:14:09 +0800

    push commit

--- src/main/java/com/tutu/git.java
+++ src/main/java/com/tutu/git.java
@@ -4,6 +4,8 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello.gi树收缩t");
         System.out.println("hello.gi树收缩t");
+        System.out.println("push test");
+
     }
 
 }
